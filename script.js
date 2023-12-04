d0bcdab5debfd8c4d9443ce0dae3add069a86db1
marcos aguilera <maroctw@hotmail.com>
2023-12-03 22:59:33 -0300

    añadi algunos efectos y musica

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let hora = document.getElementById("hora");
 let fecha = document.getElementById("fecha");
 let zonaHoraria = document.getElementById("zonaHoraria");
-
+let contenedor = document.querySelector(".contenedor");         
 
 function mostrarHora() {
   let tiempo = dayjs();
@@ -10,4 +10,17 @@ function mostrarHora() {
   fecha.innerText = tiempo.format("dddd, D MMMM, YYYY ");
 }
 
-setInterval( mostrarHora,1000)
+setInterval(mostrarHora, 1000);
+
+function obtenerColor() {
+  let red = Math.round(Math.random()*255)
+  let green = Math.round(Math.random()*255)
+  let blue = Math.round(Math.random()*255)
+  return `rgb(${red},${green},${blue})`
+}
+
+function cambiarColores() {
+  contenedor.style.backgroundColor = obtenerColor();
+}
+
+setInterval(cambiarColores, 300);
